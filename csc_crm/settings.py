4d1e8780bda11a30c3559f9eb2193775b0bb0bb1
dhanushaselvaraj645-eng <dhanusha.selvaraj05@gmail.com>
2026-04-30 22:48:13 +0530

    Fix static files config for Render deployment

--- csc_crm/settings.py
+++ csc_crm/settings.py
@@ -116,18 +116,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'csc_crm/static')
+    BASE_DIR / 'csc_crm' / 'static'
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
