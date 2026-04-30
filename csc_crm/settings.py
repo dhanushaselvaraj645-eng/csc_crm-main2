2157acd631bcc8186cd0b9ff5cd56151493c900f
dhanushaselvaraj645-eng <dhanusha.selvaraj05@gmail.com>
2026-04-30 23:06:57 +0530

    Fix static files issue

--- csc_crm/settings.py
+++ csc_crm/settings.py
@@ -125,7 +125,6 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
